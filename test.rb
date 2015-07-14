2cf2ba5da21743b7bae9f557167d45a0530ec92a
Ankesh Kothari <ankeshk@ankeshkothari.com>
2015-07-14 13:53:57 +0530

    ace points logic left

--- test.rb
+++ test.rb
@@ -81,77 +81,110 @@ player_name = gets.chomp
 
 puts " "
 total_amount = 100.00
-puts "#{player_name}: You have Rs #{total_amount} that you can bet."
 
+#loop for play again
 begin
+
+  puts "#{player_name}: You have Rs #{total_amount} that you can bet."
+
+  begin
+    puts " "
+    puts "How much would you like to bet?"
+    puts "(Bet amount has to be more than 0 but less than or equal to #{total_amount})"
+    bet_amount = gets.chomp.to_f
+  end until bet_amount <= total_amount && bet_amount > 0 
+
+  puts "#{player_name} has bet Rs #{bet_amount}"
   puts " "
-  puts "How much would you like to bet?"
-  puts "(Bet amount has to be more than 0 but less than or equal to #{total_amount})"
-  bet_amount = gets.chomp.to_f
-end until bet_amount <= total_amount && bet_amount > 0 
 
-puts "#{player_name} has bet #{bet_amount}"
-puts " "
+  player_points = 0
+  computer_points = 0
 
-player_points = 0
-computer_points = 0
-
-# start game
-# player's first hit
-player_points = hit(player_name,player_points,deck,cards_left)
-
-# computers first hit
-computer_points = hit("Computer",computer_points,deck,cards_left)
-
-# player's second hit
-player_points = hit(player_name,player_points,deck,cards_left)
-
-puts "Computer's second card is hidden"
-puts " "
-
-# ask player to hit or stay until they bust or stay
-player_bust = ""
-begin
-  puts "Enter S to Stay or H to Hit a new card"
-  player_command = gets.chomp.capitalize
-  break if player_command == "S"
-  
-  # hit a new card for player
+  # start game
+  # player's first hit
   player_points = hit(player_name,player_points,deck,cards_left)
 
-  # check if new card busted the player or not
-  player_bust = check_bust(player_points,player_name)
-end until player_command == "S" || player_bust == "yes"
-
-puts " "
-
-# computer hits or stays or busts
-computer_hit = ""
-computer_bust = ""
-begin
-  computer_hit = computer_hit_decide(player_bust,computer_bust,player_points,computer_points)
-  break if computer_hit == "no"
-
-  # hit a new card for computer
+  # computers first hit
   computer_points = hit("Computer",computer_points,deck,cards_left)
 
-  # check if new card busted the computer or not
-  computer_bust = check_bust(computer_points,"Computer")  
-end until computer_hit == "no" || computer_bust == "yes"
+  # player's second hit
+  player_points = hit(player_name,player_points,deck,cards_left)
 
-# Decide who wins and show message
-if player_bust == "yes"
-  puts "#{player_name} lost :("
-elsif computer_bust == "yes"
-  puts "#{player_name} won :)"
-elsif player_points == computer_points
-  puts "Its a tie!"
-elsif player_points > computer_points
-  puts "#{player_name} win :)"
-elsif computer_points > player_points
-  puts "#{player_name} lost :("
-end
-  
+  puts "Computer's second card is hidden"
+  puts " "
+
+  # ask player to hit or stay until they bust or stay
+  player_bust = ""
+  begin
+    puts "Enter S to Stay or H to Hit a new card"
+    player_command = gets.chomp.capitalize
+    break if player_command == "S"
     
-    
-    
+    # hit a new card for player
+    player_points = hit(player_name,player_points,deck,cards_left)
+
+    # check if new card busted the player or not
+    player_bust = check_bust(player_points,player_name)
+  end until player_command == "S" || player_bust == "yes"
+
+  puts " "
+
+  # computer hits or stays or busts
+  computer_hit = ""
+  computer_bust = ""
+  begin
+    computer_hit = computer_hit_decide(player_bust,computer_bust,player_points,computer_points)
+    break if computer_hit == "no"
+
+    # hit a new card for computer
+    computer_points = hit("Computer",computer_points,deck,cards_left)
+
+    # check if new card busted the computer or not
+    computer_bust = check_bust(computer_points,"Computer")  
+  end until computer_hit == "no" || computer_bust == "yes"
+
+  puts " "
+
+  # Decide who wins and show message
+  if player_bust == "yes"
+    puts "#{player_name} lost :("
+    player_game = "lost"
+  elsif computer_bust == "yes"
+    puts "#{player_name} won :)"
+    player_game = "won"
+  elsif player_points == computer_points
+    puts "Its a tie!"
+    player_game = "tie"
+  elsif player_points > computer_points && player_points == 21
+    puts "#{player_name} won with a BLACKJACK!"
+    player_game = "blackjack"
+  elsif player_points > computer_points
+    puts "#{player_name} won :)"
+    player_game = "won"
+  elsif computer_points > player_points
+    puts "#{player_name} lost :("
+    player_game = "lost"
+  end
+
+  # Calculate player's total amount
+  if player_game == "lost"
+    puts "#{player_name} lost Rs #{bet_amount}"
+    total_amount = total_amount - bet_amount
+  elsif player_game == "won"
+    puts "#{player_name} earns Rs #{bet_amount}"
+    total_amount = total_amount + bet_amount
+  elsif player_game == "tie"
+    total_amount = total_amount + bet_amount
+  elsif player_game == "blackjack"
+    blackjack_amount = bet_amount*1.5
+    puts "#{player_name} earns Rs #{blackjack_amount}"
+    total_amount = total_amount + blackjack_amount
+  end
+
+  puts " "
+  puts "Press P if you want to play again. Press any other key to exit."
+  play_again = gets.chomp.capitalize
+
+end while play_again == "P" 
+
+puts "Thank you #{player_name} for playing. You are leaving the table with Rs #{total_amount}."
